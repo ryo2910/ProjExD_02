49090839cd1575be3a1d15030e2b5440cb37b141
c0a22138cc <c0a22138cc@edu.teu.ac.jp>
2023-04-25 14:30:10 +0900

    練習問題3

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -15,7 +15,11 @@ def main():
     bb_img.set_colorkey((0, 0, 0))
     x, y = random.randint(0, 1600), random.randint(0, 900)
     screen.blit(bb_img, [x, y])
+    vx, vy = +1, +1
+    bb_rct = bb_img.get_rect()
+    bb_rct.center = x, y
     tmr = 0
+    
 
     while True:
         for event in pg.event.get():
@@ -25,7 +29,9 @@ def main():
         tmr += 1
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
-        screen.blit(bb_img, [x, y])
+        bb_rct.move_ip(vx, vy)
+        screen.blit(bb_img, bb_rct)
+        
 
         pg.display.update()
         clock.tick(1000)
