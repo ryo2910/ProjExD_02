be998a5efdcd3da57decd8ea11dc0d052f7b5175
c0a22138cc <c0a22138cc@edu.teu.ac.jp>
2023-04-25 14:53:11 +0900

    練習問題4

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -3,6 +3,13 @@ import sys
 
 import pygame as pg
 
+delta = {
+    pg.K_UP: (0, -1),
+    pg.K_DOWN: (0, 1),
+    pg.K_LEFT: (-1, 0),
+    pg.K_RIGHT: (1, 0)
+    }
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((1600, 900))
@@ -10,11 +17,13 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    kk_rct = kk_img.get_rect()  #練習問題4
+    kk_rct.center = 900, 400
     bb_img = pg.Surface((20, 20))
     pg.draw.circle(bb_img,(255, 0, 0), (10, 10), 10)  #爆弾の初期
     bb_img.set_colorkey((0, 0, 0))
     x, y = random.randint(0, 1600), random.randint(0, 900)
-    screen.blit(bb_img, [x, y])
+    #screen.blit(bb_img, [x, y]) 練習問題2
     vx, vy = +1, +1
     bb_rct = bb_img.get_rect()
     bb_rct.center = x, y
@@ -27,10 +36,16 @@ def main():
                 return 0
 
         tmr += 1
+
+        key_lst = pg.key.get_pressed()
+        for k, mv in delta.items():
+            if key_lst[k]:
+                kk_rct.move_ip(mv)
+
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [900, 400])
-        bb_rct.move_ip(vx, vy)
-        screen.blit(bb_img, bb_rct)
+        screen.blit(kk_img, kk_rct)  #こうかとん移動
+        bb_rct.move_ip(vx, vy)  #爆弾移動
+        screen.blit(bb_img, bb_rct)  #爆弾表示
         
 
         pg.display.update()
